7fced051848575201d13d6aa635adc2b212f2804
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-07-26 19:50:45 +0300

    Scrollbar designed

--- src/components/Sidebar/Playlists.js
+++ src/components/Sidebar/Playlists.js
@@ -1,9 +1,9 @@
 
 const Playlists = () => {
     return (
-        <nav className="mx-6 mt-2 py-2 flex-auto border-t border-white border-opacity-20 overflow-auto">
+        <nav className="mx-6 mt-2 py-2 flex-auto border-t border-white border-opacity-20 overflow-auto scrollbar">
             <ul>
-                {new Array(5).fill(<li>
+                {new Array(50).fill(<li>
                     <a href="#" className="text-s text-link hover:text-white flex h-8 items-center">
                         5. Çalma Listem
                     </a>
