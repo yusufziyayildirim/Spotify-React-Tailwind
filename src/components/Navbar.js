d7cfda76337da30ccc21e77c88ef1557523abf8a
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-07-27 00:14:04 +0300

    Navbar search component designed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import Navigation from "./Navbar/Navigation"
 import Auth from "./Navbar/Auth"
+import Search from "./Navbar/Search"
+import { useLocation } from "react-router-dom"
 
 const Navbar = () => {
+
+  const searchRoute = useLocation();
+
   return (
     <div className="h-[3.75rem] flex items-center justify-between">
       <Navigation />
+      {searchRoute.pathname=="/search" && (
+        <Search />
+      )}
+
       <Auth />
     </div>
   )
